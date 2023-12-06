c969df8b8ee54a42830efb9f18cbaea17d031f7b
JMTost <max.55@live.com.mx>
2023-12-05 23:16:43 -0600

    actualización del metodo descripcion de dietapaciente

--- components/usuariosRegistrados/profesionales/VerDietaPaciente.js
+++ components/usuariosRegistrados/profesionales/VerDietaPaciente.js
@@ -151,7 +151,190 @@ const VerDietaPaciente = ({navigation, route}) => {
 
 };
 
+const descripcion = (texto) => {
+  let cadena = "";
+  console.log(texto)
+  console.log(texto.comidasLacteos[0] === "0" )
+  //console.log(texto.comidasproteinas[0])
+  let proteinas = [], verduras = [], lacteos = [], frutas = [], granos = [];
+  if(Platform.OS === 'android'){
+  if(texto.comidasproteinas[0] != 'no aplica' && texto.comidasproteinas[0] !== '0'){
+    cadena += "Proteinas: \n";
+    if(texto.comidasproteinas[0].includes(',')){
+      let alimentosProteinas = texto.comidasproteinas[0].split(',');
+      if(alimentosProteinas.length > 1){//contamos con mas de un registro
+        let cantidadesProteinas = texto.cantidadesproteinas[0].split(',');
+        //console.log(alimentosProteinas, cantidadesProteinas);
+        for(let i = 0; i < alimentosProteinas.length; i++){
+          proteinas.push({"Alimento : ":alimentosProteinas[i], "Cantidades : " : cantidadesProteinas[i]});
+          cadena += `\t\t${alimentosProteinas[i]} : ${cantidadesProteinas[i]}\n`;
+        }
+      }
+    }else{//contamos con solo un registro
+      proteinas.push({"Alimento : ":texto.comidasproteinas[0], "Cantidades : " : texto.cantidadesproteinas[0]});
+      cadena += `\t\t${texto.comidasproteinas[0]} : ${texto.cantidadesproteinas[0]}\n`;
+    }
+  }else proteinas = "";
 
+  if(texto.comidasVerduras[0] != 'no aplica' && texto.comidasVerduras[0] !== '0'){
+    cadena += "Verduras: \n";
+    if(texto.comidasVerduras[0].includes(',')){
+      let alimentosVerduras = texto.comidasVerduras[0].split(',');
+      if(alimentosVerduras.length > 1){    
+        let cantidadesVerduras = texto.cantidadesverduras[0].split(',');
+        for(let i = 0; i < alimentosVerduras.length; i++){
+          verduras.push({"Alimento : ":alimentosVerduras[i], "Cantidades : " : cantidadesVerduras[i]});
+          cadena += `\t\t${alimentosVerduras[i]} : ${cantidadesVerduras[i]}\n`;
+        }
+      }
+    }else{
+      verduras.push({"Alimento : ":texto.comidasVerduras[0], "Cantidades : " : texto.cantidadesverduras[0]});
+      cadena += `\t\t${texto.comidasVerduras[0]} : ${texto.cantidadesverduras[0]}\n`;
+    }
+  }else verduras = "";
+
+  if(texto.comidasLacteos[0] != 'no aplica' && texto.comidasLacteos[0] !== '0'){
+    cadena += "Lacteos: \n";
+    if(texto.comidasLacteos[0].includes(',')){
+      let alimentosLacteos = texto.comidasLacteos[0].split(',');
+      if(alimentosLacteos.length > 1){
+        let cantidadesLacteos = texto.cantidadesLacteos[0].split(',');
+        for(let i = 0; i < alimentosLacteos.length; i++){
+          lacteos.push({"Alimento : ":alimentosLacteos[i], "Cantidades : " : cantidadesLacteos[i]});
+          cadena += `\t\t${alimentosLacteos[i]} : ${cantidadesLacteos[i]}\n`;
+        }
+      }
+    }else{
+      lacteos.push({"Alimento : ":texto.comidasLacteos[0], "Cantidades : " : texto.cantidadesLacteos[0]});
+      cadena += `\t\t${texto.comidasLacteos[0]} : ${texto.cantidadesLacteos[0]}\n`;
+    }
+  }else lacteos = "";
+
+  if(texto.comidasfrutas[0] != 'no aplica' && texto.comidasfrutas[0] !== '0'){
+    cadena += "Frutas: \n";
+    if(texto.comidasfrutas[0].includes(',')){
+      let alimentosFrutas = texto.comidasfrutas[0].split(',');
+      if(alimentosFrutas.length > 1){
+        let cantidadesFrutas = texto.cantidadesFrutas[0].split(',');
+        for(let i = 0; i < alimentosFrutas.length; i++){
+          frutas.push({"Alimento : ":alimentosFrutas[i], "Cantidades : " : cantidadesFrutas[i]});
+          cadena += `\t\t${alimentosFrutas[i]} : ${cantidadesFrutas[i]}\n`;
+        }
+      }
+    }else{
+      frutas.push({"Alimento : ":texto.comidasfrutas[0], "Cantidades : " : texto.cantidadesFrutas[0]});
+      cadena += `\t\t${texto.comidasfrutas[0]} : ${texto.cantidadesFrutas[0]}\n`;
+    }
+  }else frutas = "";
+
+  if(texto.comidasgranos[0] != 'no aplica' && texto.comidasgranos[0] !== '0'){
+    cadena += "Granos: \n";
+    if(texto.comidasgranos[0].includes(',')){
+      let alimentosGranos = texto.comidasgranos[0].split(',');
+      if(alimentosGranos.length > 1){
+        let cantidadesGranos = texto.cantidadesGranos[0].split(',');
+        for(let i = 0; i < alimentosGranos.length; i++){
+          granos.push({"Alimento : ":alimentosGranos[i], "Cantidades : " : cantidadesGranos[i]});
+          cadena += `\t\t${alimentosGranos[i]} : ${cantidadesGranos[i]}\n`;
+        }
+      }
+    }else{
+      granos.push({"Alimento : ":texto.comidasgranos[0], "Cantidades : " : texto.cantidadesGranos[0]});
+      cadena += `\t\t${texto.comidasgranos[0]} : ${texto.cantidadesGranos[0]}\n`;
+    }
+  }else granos = "";
+    Alert.alert("Descripción.", cadena, [{text : 'Cerrar'}]);
+  }else if(Platform.OS === 'ios'){
+    if(texto.comidasproteinas[0] != 'no aplica' && texto.comidasproteinas[0] !== '0'){
+      cadena += "Proteinas: \n";
+      if(texto.comidasproteinas[0].includes(',')){
+        console.log("data")
+        let alimentosProteinas = texto.comidasproteinas[0].split(',');
+        if(alimentosProteinas.length > 1){//contamos con mas de un registro
+          let cantidadesProteinas = texto.cantidadesproteinas[0].split(',');
+          //console.log(alimentosProteinas, cantidadesProteinas);
+          for(let i = 0; i < alimentosProteinas.length; i++){
+            proteinas.push({"Alimento : ":alimentosProteinas[i], "Cantidades : " : cantidadesProteinas[i]});
+            cadena += `${alimentosProteinas[i]} : ${cantidadesProteinas[i]}\n`;
+          }
+        }
+      }else{//contamos con solo un registro
+        proteinas.push({"Alimento : ":texto.comidasproteinas[0], "Cantidades : " : texto.cantidadesproteinas[0]});
+        cadena += `${texto.comidasproteinas[0]} : ${texto.cantidadesproteinas[0]}\n`;
+      }
+    }else proteinas = "";
+  
+    if(texto.comidasVerduras[0] != 'no aplica' && texto.comidasVerduras[0] !== '0'){
+      cadena += "Verduras: \n";
+      if(texto.comidasVerduras[0].includes(',')){
+        let alimentosVerduras = texto.comidasVerduras[0].split(',');
+        if(alimentosVerduras.length > 1){    
+          let cantidadesVerduras = texto.cantidadesverduras[0].split(',');
+          for(let i = 0; i < alimentosVerduras.length; i++){
+            verduras.push({"Alimento : ":alimentosVerduras[i], "Cantidades : " : cantidadesVerduras[i]});
+            cadena += `${alimentosVerduras[i]} : ${cantidadesVerduras[i]}\n`;
+          }
+        }
+      }else{
+        verduras.push({"Alimento : ":texto.comidasVerduras[0], "Cantidades : " : texto.cantidadesverduras[0]});
+        cadena += `${texto.comidasVerduras[0]} : ${texto.cantidadesverduras[0]}\n`;
+      }
+    }else verduras = "";
+  
+    if(texto.comidasLacteos[0] != 'no aplica' && texto.comidasLacteos[0] !== '0'){
+      cadena += "Lacteos: \n";
+      if(texto.comidasLacteos[0].includes(',')){
+        let alimentosLacteos = texto.comidasLacteos[0].split(',');
+        if(alimentosLacteos.length > 1){
+          let cantidadesLacteos = texto.cantidadesLacteos[0].split(',');
+          for(let i = 0; i < alimentosLacteos.length; i++){
+            lacteos.push({"Alimento : ":alimentosLacteos[i], "Cantidades : " : cantidadesLacteos[i]});
+            cadena += `${alimentosLacteos[i]} : ${cantidadesLacteos[i]}\n`;
+          }
+        }
+      }else{
+        lacteos.push({"Alimento : ":texto.comidasLacteos[0], "Cantidades : " : texto.cantidadesLacteos[0]});
+        cadena += `${texto.comidasLacteos[0]} : ${texto.cantidadesLacteos[0]}\n`;
+      }
+    }else lacteos = "";
+  
+    if(texto.comidasfrutas[0] != 'no aplica' && texto.comidasfrutas[0] !== '0'){
+      cadena += "Frutas: \n";
+      if(texto.comidasfrutas[0].includes(',')){
+        let alimentosFrutas = texto.comidasfrutas[0].split(',');
+        if(alimentosFrutas.length > 1){
+          let cantidadesFrutas = texto.cantidadesFrutas[0].split(',');
+          for(let i = 0; i < alimentosFrutas.length; i++){
+            frutas.push({"Alimento : ":alimentosFrutas[i], "Cantidades : " : cantidadesFrutas[i]});
+            cadena += `${alimentosFrutas[i]} : ${cantidadesFrutas[i]}\n`;
+          }
+        }
+      }else{
+        frutas.push({"Alimento : ":texto.comidasfrutas[0], "Cantidades : " : texto.cantidadesFrutas[0]});
+        cadena += `${texto.comidasfrutas[0]} : ${texto.cantidadesFrutas[0]}\n`;
+      }
+    }else frutas = "";
+  
+    if(texto.comidasgranos[0] != 'no aplica' && texto.comidasgranos[0] !== '0'){
+      cadena += "Granos: \n";
+      if(texto.comidasgranos[0].includes(',')){
+        let alimentosGranos = texto.comidasgranos[0].split(',');
+        if(alimentosGranos.length > 1){
+          let cantidadesGranos = texto.cantidadesGranos[0].split(',');
+          for(let i = 0; i < alimentosGranos.length; i++){
+            granos.push({"Alimento : ":alimentosGranos[i], "Cantidades : " : cantidadesGranos[i]});
+            cadena += `${alimentosGranos[i]} : ${cantidadesGranos[i]}\n`;
+          }
+        }
+      }else{
+        granos.push({"Alimento : ":texto.comidasgranos[0], "Cantidades : " : texto.cantidadesGranos[0]});
+        cadena += `${texto.comidasgranos[0]} : ${texto.cantidadesGranos[0]}\n`;
+      }
+    }else granos = "";
+    Alert.alert("Descripción.", cadena, [{text : 'Cerrar'}], []);
+  }
+}
+/*
 const descripcion = (texto) => {
     //console.log("datos: ", texto)
     let cadena = "";
@@ -334,7 +517,7 @@ const descripcion = (texto) => {
       Alert.alert("Descripción.", cadena, [{text : 'Cerrar'}], []);
     }
   }
-  
+*/
 
 const styles = StyleSheet.create({
     container: {
